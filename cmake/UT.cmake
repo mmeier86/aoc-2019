8a3b396f70fa1938a6c1a677cccad344e0f95bbf
Michael Meier <mmeier1986@googlemail.com>
2019-11-30 17:40:48 +0100

    Toolkit: Add UT CMake macros with Catch2
    Catch2 is the choosen UT framework this season. The UT.cmake toolkit
    adds the add_ut macro, mostly copied from last year, for adding unit
    tests, using the Catch2 framework. It'll only be enabled with the
    UNITTESTS_ENABLED=ON CMake option.
    
    If Valgrind is available, the toolkit will also generate a target
    for executing UTs under Valgrind.
    
    The target for running all non-valgrind UTs in a build is "check".
    With the "check_valg" target, all UTs will be executed under Valgrind
    to identify memory and such.

---
+++ cmake/UT.cmake
@@ -0,0 +1,62 @@
+# UT functionality for AoC 2019
+
+include_guard(GLOBAL)
+
+option(UNITTESTS_ENABLED
+  "ENABLE Unit Test Functionality"
+  OFF
+  )
+
+if(UNITTESTS_ENABLED)
+  enable_testing()
+  set(VALGRIND_LABEL "Valgrind")
+  find_program(VALGRIND_CMD valgrind)
+  if(NOT VALGRIND_CMD)
+    message(INFO "Valgrind not found. Valgrind UTs will not be genrated.")
+  else()
+    add_custom_target(check_valg
+      COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure -L "${VALGRIND_LABEL}"
+      )
+  endif()
+  add_custom_target(check
+    COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure -LE "${VALGRIND_LABEL}"
+    )
+
+  get_filename_component(CATCH_DIR ${CMAKE_CURRENT_LIST_DIR}/../third_party/catch/ ABSOLUTE)
+
+  add_library(catch2 INTERFACE)
+  target_include_directories(catch2
+    INTERFACE
+    ${CATCH_DIR}
+    )
+  target_compile_definitions(catch2
+    INTERFACE
+    CATCH_CONFIG_MAIN
+    )
+
+  macro(add_ut t_name t_file)
+    cmake_parse_arguments(ADDUT "" "T_DIR" "" ${ARGN})
+    add_executable(${t_name} ${t_file} ${ADDUT_UNPARSED_ARGUMENTS})
+    target_compile_definitions(${t_name} PRIVATE UNITTESTS_ENABLED)
+    target_link_libraries(${t_name} PRIVATE catch2)
+    add_test(NAME ${t_name}
+      COMMAND ${t_name} --use-colour yes ${ADDUT_T_DIR}
+      )
+    add_dependencies(check ${t_name})
+    if(VALGRIND_CMD)
+      add_test(NAME ${t_name}_valgrind
+        COMMAND ${VALGRIND_CMD} --error-exitcode=1 --leak-check=full --track-origins=yes $<TARGET_FILE:${t_name}> --use-colour yes ${ADDUT_T_DIR}
+        )
+      set_tests_properties(${t_name}_valgrind PROPERTIES LABELS "${VALGRIND_LABEL}")
+      add_dependencies(check_valg ${t_name})
+    endif()
+  endmacro()
+  macro(link_ut t_name)
+    target_link_libraries(${t_name} ${ARGN})
+  endmacro()
+else()
+  macro(add_ut)
+  endmacro()
+  macro(link_ut)
+  endmacro()
+endif()
