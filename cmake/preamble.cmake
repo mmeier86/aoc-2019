3dcc7a56b6c6d139bdf59ffa6eac4f21c24953e3
Michael Meier <mmeier1986@googlemail.com>
2019-11-30 17:44:03 +0100

    Toolkit: Add CMake preamble with common settings
    The preamble.cmake file contains some common settings for this year's
    Advent of Code. It sets the required C++ standard to C++17, enables
    the export of compile commands for IDE usage and includes the UT.cmake
    UT toolkit.
    
    Furthermore, -Wall, -Werror and -Wextra are added to the compile flags
    when the build type is Debug.

---
+++ cmake/preamble.cmake
@@ -0,0 +1,15 @@
+# A common preamble for all AoC 2019 projects
+
+include_guard(GLOBAL)
+
+get_filename_component(ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/.. ABSOLUTE)
+
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+string(APPEND CMAKE_CXX_FLAGS_DEBUG " -Wall -Werror -Wextra")
+
+list(APPEND CMAKE_MODULE_PATH ${ROOT_DIR}/cmake)
+
+include(UT)
